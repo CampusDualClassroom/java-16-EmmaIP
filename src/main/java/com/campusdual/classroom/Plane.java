81106c0ae90ada8f5ea83d635232a1e4638d93d2
EmmaIP <emmapineiro@gmail.com>
2024-10-17 15:45:04 +0200

    ejercicio 16 finalizado

--- src/main/java/com/campusdual/classroom/Plane.java
+++ src/main/java/com/campusdual/classroom/Plane.java
@@ -1,22 +1,44 @@
 package com.campusdual.classroom;
 
-public class Plane{
+public class Plane implements IMachine{
 
 	private final String name;
 
 	public Plane(String name) {
+
 		this.name = name;
 	}
 
+	public String getName() {
+		return name;
+	}
+
 	public void takeOff() {
-		System.out.println("El avión despega");
+
+		System.out.println("El avión " + this.name + " despega");
 	}
 
 	public void land() {
-		System.out.println("El avión aterriza");
+
+		System.out.println("El avión " + this.name + " aterriza");
 	}
 
 	public void fly() {
-		System.out.println("El avión está volando");
+		System.out.println("El avión " + this.name + " está volando");
+	}
+
+	@Override
+	public void start() {
+		System.out.println("El avión " + this.name + " está encendido");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("El avión " + this.name + " está apagado");
+	}
+
+	@Override
+	public void maintenance() {
+		System.out.println("El avión " + this.name + " está realizando un mantenimiento");
 	}
 }
